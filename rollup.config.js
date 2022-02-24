0622e966965c17b065d100c8510c7c3f1bfc00b8
Flavio Silva <55815265+201flaviosilva@users.noreply.github.com>
2022-02-24 14:39:09 +0000

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,13 @@ import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import babel from "rollup-plugin-babel";
 
+const forder = "_ignore/Build/";
+// const forder = "dist/";
 const umd = {
-	input: "src/Impacto.js",
+	input: forder + "Impacto.js",
 	output: {
 		name: "Impacto",
-		file: "dist/Impacto.umd.js", // Universal Module Definition
+		file: forder + "Impacto.umd.js", // Universal Module Definition
 		format: "umd",
 	},
 	plugins: [
@@ -23,11 +25,11 @@ export default [
 	{
 		input: "src/Impacto.js",
 		output: [ // https://betterprogramming.pub/what-are-cjs-amd-umd-esm-system-and-iife-3633a112db62
-			{ file: "dist/Impacto.esm.js", format: "esm" }, // ES Module
-			{ file: "dist/Impacto.cjs.js", format: "cjs" }, // CommonJS
-			// { file: "dist/Impacto.amd.js", format: "amd" }, // Asynchronous Module Definition
-			// { file: "dist/Impacto.system.js", format: "system" }, // 
-			// { file: "dist/Impacto.iife.js", format: "iife" }, // IIFE (vanilla) probably useless
+			{ file: forder + "Impacto.esm.js", format: "esm" }, // ES Module
+			{ file: forder + "Impacto.cjs.js", format: "cjs" }, // CommonJS
+			// { file: forder + "Impacto.amd.js", format: "amd" }, // Asynchronous Module Definition
+			// { file: forder + "Impacto.system.js", format: "system" }, // 
+			// { file: forder + "Impacto.iife.js", format: "iife" }, // IIFE (vanilla) probably useless
 		],
 		plugins: [
 			babel({
