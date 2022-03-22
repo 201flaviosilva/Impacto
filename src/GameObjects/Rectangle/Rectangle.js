9e34483bce672a65ca144e5d463757caa5a4888c
Flavio Silva <55815265+201flaviosilva@users.noreply.github.com>
2022-03-22 18:39:13 +0000

    [Engine]: Scale Game Object #32
    #32 [Engine]: Scale Game Object

--- src/GameObjects/Rectangle/Rectangle.js
+++ src/GameObjects/Rectangle/Rectangle.js
@@ -13,8 +13,8 @@ export default class Rectangle extends GameObject {
 		this._type = Types.rectangle;
 	}
 
-	get x() { return this._x - this.width * this.origin.x; } // Get the position X relative to the origin
-	get y() { return this._y - this.height * this.origin.y; } // Get the position Y relative to the origin
+	get x() { return this._x - this.width * this.origin.x * this.scale.x; } // Get the position X relative to the origin
+	get y() { return this._y - this.height * this.origin.y * this.scale.y; } // Get the position Y relative to the origin
 }
 
 Object.assign(Rectangle.prototype, CommonMethods);
