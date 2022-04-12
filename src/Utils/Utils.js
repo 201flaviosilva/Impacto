5646df542b7cbbf492d0ff8beb197bfa05b0e705
Flavio Silva <55815265+201flaviosilva@users.noreply.github.com>
2022-04-12 17:45:16 +0100

    0.9.0-0

--- src/Utils/Utils.js
+++ src/Utils/Utils.js
@@ -5,7 +5,7 @@ export default class Utils {
 		this.Math = new ImpactoMath();
 	}
 
-	getVersion() { return "Impacto: 0.8.1"; }
+	getVersion() { return "Impacto: 0.9.0-0"; }
 }
 
 export const UtilsInstance = new Utils();
