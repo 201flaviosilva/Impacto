fc55744e7b90b3ee504ddcbe72c3108d429e91e6
Flavio Silva <55815265+201flaviosilva@users.noreply.github.com>
2022-04-21 18:53:15 +0100

    [Doc]: Documenting the Rectangle (js doc)

--- src/GameObjects/GameObjects.js
+++ src/GameObjects/GameObjects.js
@@ -1,4 +1,3 @@
-// Draw
 import GameObjectBase from "./GameObject2D.js";
 import AudioPlay from "./AudioPlay/AudioPlay.js";
 import Circle from "./Circle/Circle.js";
@@ -9,14 +8,28 @@ import Sprite from "./Sprite/Sprite.js";
 import Text from "./Text/Text.js";
 import Triangle from "./Triangle/Triangle.js";
 
-
-// Others
 import Types from "./Types.js";
 
+/**
+ * @namespace Impacto.GameObjects
+ * 
+ * @property {Impacto.GameObjects.GameObjectBase} GameObjectBase
+ * 
+ * @property {Impacto.GameObjects.AudioPlay} AudioPlay
+ * @property {Impacto.GameObjects.Circle} Circle
+ * @property {Impacto.GameObjects.Line} Line
+ * @property {Impacto.GameObjects.Polygon} Polygon
+ * @property {Impacto.GameObjects.Rectangle} Rectangle
+ * @property {Impacto.GameObjects.Sprite} Sprite
+ * @property {Impacto.GameObjects.Text} Text
+ * @property {Impacto.GameObjects.Triangle} Triangle
+ * 
+ * @property {Impacto.GameObjects.Types} Types
+ */
 export default class GameObjects {
 	constructor() {
-		// Basic GameObject
 		this.GameObjectBase = GameObjectBase;
+
 		this.AudioPlay = AudioPlay;
 		this.Circle = Circle;
 		this.Line = Line;
@@ -26,9 +39,7 @@ export default class GameObjects {
 		this.Text = Text;
 		this.Triangle = Triangle;
 
-		// Others
 		this.Types = Types;
 	}
 }
-
 export const GameObjectsInstance = new GameObjects();
