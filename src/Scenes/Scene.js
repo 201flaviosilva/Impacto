40656a7553208566efba5e57569200b0134047ca
Flavio Silva <55815265+201flaviosilva@users.noreply.github.com>
2022-05-09 19:27:04 +0100

    [Engine]: Documenting the Scene and Utils (JS Doc) #65

--- src/Scenes/Scene.js
+++ src/Scenes/Scene.js
@@ -1,3 +1,14 @@
+/**
+ * @classdesc
+ * Create a new scene
+ * 
+ * @example
+ * class MyScene extends Scene {}
+ * 
+ * @class Scene
+ * @memberof Impacto
+ * @constructors 
+ */
 export default class Scene {
 	constructor() {
 		this.children = [];
@@ -13,16 +24,83 @@ export default class Scene {
 		}
 	}
 
+	/**
+	 * @description
+	 * Add a child to the scene
+	 * 
+	 * @example
+	 * this.addChild(mySprite);
+	 * 
+	 * @param {Impacto.GameObject} child - The child to add
+	 * @returns {Impacto.Scene} This scene
+	 * @memberof Impacto.Scene
+	 */
 	addChild(child) {
 		this.children.push(child);
 	}
 
+	/**
+	 * @description
+	 * Remove a child from the scene
+	 * 
+	 * @example
+	 * this.removeChild(mySprite);
+	 * 
+	 * @param {Impacto.GameObject} child - The child to remove
+	 * @returns {Impacto.Scene} This scene
+	 * @memberof Impacto.Scene
+	 */
 	removeChild(child) {
 		const index = this.children.indexOf(child);
 		if (index > -1) this.children.splice(index, 1);
 	}
 
+	/**
+	 * @description
+	 * The entry point of the scene, this function is called once when the scene is loaded
+	 * Normally, you use this function to create and add your game objects
+	 * 
+	 * @example
+	 * class MyScene extends Scene {
+	 *  	start() {
+	 * 		const sprite = new Sprite();
+	 * 		this.addChild(sprite);
+	 * 	}
+	 * }
+	 *
+	 * @memberof Impacto.Scene
+	 */
 	start() { }
+
+	/**
+	 * @description
+	 * The update function is called every frame
+	 * 
+	 * @example
+	 * class MyScene extends Scene {
+	 * 	update(delta) {
+	 *    console.log(delta);
+	 * 	}
+	 * }
+	 * 
+	 * @param {number} delta - The time since the last frame
+	 * @memberof Impacto.Scene
+	 */
 	update(delta) { }
+
+	/**
+	 * @description
+	 * The render function is called every frame after the update and core render function
+	 * 
+	 * @example
+	 * class MyScene extends Scene {
+	 * 	render(context) {
+	 * 	  console.log("render");
+	 * 	}
+	 * }
+	 * 
+	 * @param {CanvasRenderingContext2D} context - The canvas context
+	 * @memberof Impacto.Scene
+	 */
 	posRender(context) { }
 }
--- src/Utils/Utils.js
+++ src/Utils/Utils.js
@@ -1,10 +1,26 @@
 import ImpactoMath from "./Math.js";
 
+/**
+ * @namespace Impacto.Utils
+ * 
+ * @property {Impacto.Utils.Math} Math
+ */
 export default class Utils {
 	constructor() {
 		this.Math = new ImpactoMath();
 	}
 
+	/**
+	 * @description 
+	 * Returns teh current version of the library
+	 * 
+	 * @example
+	 * console.log(Impacto.Utils.getVersion());
+	 * 
+	 * @returns {String}
+	 * @memberof Impacto.Utils
+	 * @method getVersion
+	 */
 	getVersion() { return "Impacto: 0.9.0-0"; }
 }
 
