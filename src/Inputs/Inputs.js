08647ce89ab6b9a2638061f8b3d49e963de6fcc3
Flavio Silva <55815265+201flaviosilva@users.noreply.github.com>
2022-05-16 18:29:08 +0100

    [Engine]: Documenting the Mouse input (JS Doc) #65

--- src/Inputs/Inputs.js
+++ src/Inputs/Inputs.js
@@ -1,10 +1,16 @@
 import KeyBoard from "./Keyboard.js";
-import Mouse from "./Mouse.js";
+import MouseManager from "./Mouse.js";
 
+/**
+ * @class Inputs
+ * @description Inputs class
+ * @memberof Impacto
+ * @instance
+ */
 export default class Inputs {
 	constructor() {
 		this.KeyBoard = new KeyBoard();
-		this.Mouse = new Mouse();
+		this.Mouse = new MouseManager();
 	}
 }
 
