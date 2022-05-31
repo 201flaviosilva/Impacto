c1df1e6f7b51a79c6471680afdd5588e6dedf2ee
Flavio Silva <55815265+201flaviosilva@users.noreply.github.com>
2022-05-31 22:17:38 +0100

    Create: tsconfig.json

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,12 @@ const path = require("path");
 module.exports = {
 	mode: "production",
 	entry: "./src/Impacto.js",
-	optimization: {
-		minimize: false,
-	},
-	experiments: {
-		outputModule: true,
-	},
+	optimization: { minimize: false, },
+	experiments: { outputModule: true, },
 	output: {
 		path: path.resolve("dist"),
 		filename: "Impacto.js",
-		libraryTarget: "module", // module/commonjs2 // https://webpack.js.org/configuration/output/#outputlibrarytarget
+		libraryTarget: "module", // module/commonjs2/window // https://webpack.js.org/configuration/output/#outputlibrarytarget
 	},
 	module: {
 		rules: [
